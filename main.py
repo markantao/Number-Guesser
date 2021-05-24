2fd6bdfeab454274cadabb393c20bd423f624997
Mark Antao <35659231+markantao@users.noreply.github.com>
2021-05-23 23:23:54 -0400

    Got the basics of the game down

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-# Number Guesser
+import random
 
 # Steps:
 
 # Get random number
-
+for x in range(1):
+    numRandom = random.randint(1, 11)
+    print(numRandom)
 # Ask user for input
-
+question = input("Guess a number from 1-10?: ")
 # Check if input is correct
+if question == numRandom:
+    print("Awesome Job! You Win!")
+elif question != numRandom:
+    print("Aw! So close! Try again next time!")
+else:
+    print("Sorry, I did not understand!")
